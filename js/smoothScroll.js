97e57f98e3ddb0cae3af220a400178da25c01c38
ApplexShampoox <axsxbarker@gmail.com>
2021-06-01 21:12:10 +0200

    tabs, modal and menu
    Добавлены табы, модальное окно и бургер-меню

--- js/smoothScroll.js
+++ js/smoothScroll.js
@@ -1,13 +1,39 @@
-const smoothScrollElems = document.querySelectorAll('a[href^="#"]:not(a[href="#"])');
+const SPEED = 0.5;
+const scrolled = e => {
+    e.preventDefault();
 
-smoothScrollElems.forEach(link => {
-  link.addEventListener('click', (event) => {
+    const target = e.target;
     
-    event.preventDefault()
-    const id = link.getAttribute('href').substring(1)
+    if (target.matches('[href^="#"]')) {
+        let start = 0;
+        const pageY = window.pageYOffset;
 
-    document.getElementById(id).scrollIntoView({
-      behavior: 'smooth'
-    });
-  })
-})
+        const hash = target.getAttribute('href');
+
+        if (hash === '#') return
+
+        const elem = document.querySelector(hash);
+
+        const coordinateElem = elem.getBoundingClientRect().top;
+
+        const step = time => {
+            if (!start) start = time;
+            const progress = time - start;
+
+            const r = (coordinateElem < 0 ?
+                Math.max(pageY - progress / SPEED, pageY + coordinateElem) :
+                Math.min(pageY + progress / SPEED, pageY + coordinateElem))
+
+
+            window.scrollTo(0, r);
+
+            if (r < pageY + coordinateElem) requestAnimationFrame(step);
+            
+        }
+
+        requestAnimationFrame(step)
+        
+    }
+}
+
+document.body.addEventListener('click', scrolled);
